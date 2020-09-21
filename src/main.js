cc42664b6d182911c3c16609a6e57980cf9194a9
liaomin <369585228@qq.com>
2020-09-21 23:51:25 +0800

    第一次主分支提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+import axios from 'axios'
+// 导入全局样式表
+import './assets/css/global.css'
 
+axios.defaults.baseURL = 'http://127.0.0.1:5000'
+Vue.prototype.axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
